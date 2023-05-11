05ec999115ac20827ddd70712f993fea0424f8cb
Sahil Narula <126839546+Hunkhill1@users.noreply.github.com>
2023-05-11 14:02:11 +0800

    Added comments + checking shorestPath logic

--- CITS2200-Project/MyCITS2200Project.java
+++ CITS2200-Project/MyCITS2200Project.java
@@ -6,6 +6,8 @@ public class MyCITS2200Project implements CITS2200Project{
     private Map <String, Integer> urlIDs;
     private int nodeCount;
     private ArrayList<ArrayList<Integer>> adjacencyList;
+	//look up how hashmaps work
+	//understand how arraylists work and one within another
 
     public MyCITS2200Project(){
         adjacencyList = new ArrayList<ArrayList<Integer>>();
@@ -25,11 +27,12 @@ public class MyCITS2200Project implements CITS2200Project{
         //check
         boolean nodeFromExists = urlIDs.containsKey(urlFrom);
         boolean nodeToExists = urlIDs.containsKey(urlTo);
+		//find out what contains key does
 
-        if(!nodeFromExists){
+        if(!nodeFromExists){  //if the node does not exist, put it into our hashmap and have the counter ready
             urlIDs.put(urlFrom,nodeCount);
-            adjacencyList.add(new ArrayList<Integer>());
-            nodeCount++;
+            adjacencyList.add(new ArrayList<Integer>()); //We are also adding it to our arraylist
+            nodeCount++; //incrementing the counter
         }
         if(!nodeToExists){
             urlIDs.put(urlTo,nodeCount);
@@ -37,16 +40,16 @@ public class MyCITS2200Project implements CITS2200Project{
             nodeCount++;
         }
 
-        int currentNodeID = urlIDs.get(urlFrom);
-        ArrayList<Integer> currentNode;
+        int currentNodeID = urlIDs.get(urlFrom); //This is getting the url from index?
+        ArrayList<Integer> currentNode;			//we are making an array list object
 
-        currentNode=adjacencyList.get(currentNodeID);
+        currentNode=adjacencyList.get(currentNodeID);  //and we are getting that node
         
         
         //adding nodes that are adjacent to currentNode
-        int adjnodeID = urlIDs.get(urlTo);
-        currentNode.add(adjnodeID);
-        //adjacencyList.add(currentNodeID,currentNode); 
+        int adjnodeID = urlIDs.get(urlTo); //OOHHH I GET IT
+        currentNode.add(adjnodeID);    //if a node exists we get that ID number and thenwe add to it
+        //adjacencyList.add(currentNodeID,currentNode);  //fucking hell lance
 
 
         //FIXME: FOR TESTING PURPOSES only
@@ -71,7 +74,15 @@ public class MyCITS2200Project implements CITS2200Project{
 	 * @return the legnth of the shorest path in number of links followed.
 	 */
 	public int getShortestPath(String urlFrom, String urlTo){  // needs to be breath first search using a queue
-        return 0;
+        //urlFRom == Starting vertex
+		//UrlTo == ending vertex
+		/*
+		 *
+		 * TODO figure out a way to get the index of the starting vertex and traverse down it 
+		 * to the ending vertex --> use the hashmap to find out the index, and 
+		 * figure out and understand how to use a breath-first-search
+		 */
+		return 0;
     }
 
 	/**
